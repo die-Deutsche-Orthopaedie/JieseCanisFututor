e0e003967e083176f5695a34d92ccc01fe12f07e
die♂Deutsche♂Orthopädie <amo@ludus.z0d.eu>
2015-11-20 17:47:33 +0800

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -4,7 +4,22 @@ do
 	currentdate=`echo $pair | cut -d♂ -f2`
 	password=`echo $pair | cut -d♂ -f3`
 	# /root/rar/rar x -ts -p"$password" "$folder.$currentdaterar"
-	/root/rar/rar x -df -ts -p"$password" "$folder.$currentdate.rar"
+	if [ ! -f "$folder.$currentdate.rar" ]
+	then
+		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.rar"
+	fi
+	if [ ! -f "$folder.$currentdate.part1.rar" ]
+	then
+		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.part1.rar"
+	fi
+	if [ ! -f "$folder.$currentdate.part01.rar" ]
+	then
+		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.part01.rar"
+	fi
+	if [ ! -f "$folder.$currentdate.part001.rar" ]
+	then
+		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.part001.rar"
+	fi
 	cd $folder
 	md5sum --quiet -c $folder.md5
 	sha1sum --quiet -c $folder.sha1
