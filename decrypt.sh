96834857b0b45fab5c045beafd229ff016023b8a
die♂Deutsche♂Orthopädie <amo@ludus.z0d.eu>
2015-11-24 23:11:27 +0800

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -6,19 +6,19 @@ do
 	currentdate=`echo $pair | cut -d♂ -f2`
 	password=`echo $pair | cut -d♂ -f3`
 	# /root/rar/rar x -ts -p"$password" "$folder.$currentdaterar"
-	if [ ! -f "$folder.$currentdate.rar" ]
+	if [ -f "$folder.$currentdate.rar" ]
 	then
 		$RARpath x -ts -p"$password" "$folder.$currentdate.rar"
 	fi
-	if [ ! -f "$folder.$currentdate.part1.rar" ]
+	if [ -f "$folder.$currentdate.part1.rar" ]
 	then
 		$RARpath x -ts -p"$password" "$folder.$currentdate.part1.rar"
 	fi
-	if [ ! -f "$folder.$currentdate.part01.rar" ]
+	if [ -f "$folder.$currentdate.part01.rar" ]
 	then
 		$RARpath x -ts -p"$password" "$folder.$currentdate.part01.rar"
 	fi
-	if [ ! -f "$folder.$currentdate.part001.rar" ]
+	if [ -f "$folder.$currentdate.part001.rar" ]
 	then
 		$RARpath x -ts -p"$password" "$folder.$currentdate.part001.rar"
 	fi
