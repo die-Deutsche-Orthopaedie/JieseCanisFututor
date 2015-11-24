f705c239056366aa6cc56e4f573ac77e35f68c09
die♂Deutsche♂Orthopädie <amo@ludus.z0d.eu>
2015-11-24 22:58:49 +0800

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,12 +1,14 @@
 function GeneratePW { 
 	len=64
-	str=(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 短 发 大 法 好 \! \@ \# \$ \% \^ \& \* \( \) \- \_ \= \+ \\ \/ \' \" \; \: \[ \] \{ \} \, \. \?)
+	str=(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 短 发 大 法 好 \! \@ \# \$ \% \^ \& \* \( \) \- \_ \= \+ \\ \/ \; \: \[ \] \{ \} \, \. \?)
 	for((i=1;i<=$len;i++))
 	do
 		arr[$i]=${str[$[$RANDOM%87]]}
 	done
 	echo "${arr[@]}"|tr -d " "
 }
+
+RARpath="/root/rar/rar"
 
 echo "" > pair.txt
 for folder in `ls`
@@ -28,7 +30,7 @@ do
 		password=`GeneratePW`
 		# /root/rar/rar a -htb -m0 -ma5 -rr5 -ts -hp"$password" "$folder.rar" $folder
 		currentdate=`date +%y.%m.%d`
-		/root/rar/rar a -df -v2.33g -v1g -htb -m0 -ma5 -rr5 -ts -hp"$password" "$folder.$currentdate.rar" $folder
+		$RARpath a -df -v2.33g -v1g -htb -m0 -ma5 -rr5 -ts -hp"$password" "$folder.$currentdate.rar" $folder
 		echo "$folder♂$currentdate♂$password" > "pair.$folder.$currentdate.txt"
 		echo "$folder♂$currentdate♂$password" >> pair.txt
 	fi
