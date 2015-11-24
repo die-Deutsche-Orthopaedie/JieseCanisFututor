fc91df1ce7cb064f338abdaf4ffc336452520d49
die♂Deutsche♂Orthopädie <amo@ludus.z0d.eu>
2015-11-24 23:02:46 +0800

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -1,3 +1,5 @@
+RARpath="/root/rar/rar"
+
 for pair in `cat pair.txt`
 do
 	folder=`echo $pair | cut -d♂ -f1`
@@ -6,19 +8,19 @@ do
 	# /root/rar/rar x -ts -p"$password" "$folder.$currentdaterar"
 	if [ ! -f "$folder.$currentdate.rar" ]
 	then
-		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.rar"
+		$RARpath x -ts -p"$password" "$folder.$currentdate.rar"
 	fi
 	if [ ! -f "$folder.$currentdate.part1.rar" ]
 	then
-		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.part1.rar"
+		$RARpath x -ts -p"$password" "$folder.$currentdate.part1.rar"
 	fi
 	if [ ! -f "$folder.$currentdate.part01.rar" ]
 	then
-		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.part01.rar"
+		$RARpath x -ts -p"$password" "$folder.$currentdate.part01.rar"
 	fi
 	if [ ! -f "$folder.$currentdate.part001.rar" ]
 	then
-		/root/rar/rar x -ts -p"$password" "$folder.$currentdate.part001.rar"
+		$RARpath x -ts -p"$password" "$folder.$currentdate.part001.rar"
 	fi
 	cd $folder
 	md5sum --quiet -c $folder.md5
