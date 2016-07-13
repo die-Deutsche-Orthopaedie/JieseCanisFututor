07293310a2cfb3fb012e15472173b73ea29df207
die♂Deutsche♂Orthopädie♂spezialist <amo@ludus.z0d.eu>
2016-07-13 11:40:09 +0800

    Create encrypt.v3.sh
    compatible with cygwin, now you can use the script on Windows. and don't forget to copy a WinRAR into cygwin folder. 
    兼容cygwin，现在你可以在Windows上面使用这个脚本了。还有别忘了往cygwin里面拷一份WinRAR。

---
+++ encrypt.v3.sh
@@ -0,0 +1,48 @@
+function GeneratePW { 
+	len=64
+	str=(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 短 发 大 法 好 \! \@ \# \$ \% \^ \& \* \( \) \- \_ \= \+ \\ \/ \' \; \: \[ \] \{ \} \, \. \?)
+	for((i=1;i<=$len;i++))
+	do
+		arr[$i]=${str[$[$RANDOM%87]]}
+	done
+	echo "${arr[@]}"|tr -d " "
+}
+
+RARpath="/home/ddOs/WinRAR/rar"
+
+function encrypt {
+	if [ -d $1 ]
+	then
+		cd $1
+		# md5sum *.* > /tmp/md5
+		echo "Caculatin' MD5sum, please wait... "
+		find  -type f -print0 | xargs -0 md5sum > /tmp/$1.md5
+		# sha1sum *.* > /tmp/sha1
+		echo "Caculatin' SHA1sum, please wait... "
+		find  -type f -print0 | xargs -0 sha1sum > /tmp/$1.sha1
+		# sha256sum *.* > /tmp/sha256
+		echo "Caculatin' SHA256sum, please wait... "
+		find  -type f -print0 | xargs -0 sha256sum > /tmp/$1.sha256
+		mv /tmp/$1.md5 $1.md5
+		mv /tmp/$1.sha1 $1.sha1
+		mv /tmp/$1.sha256 $1.sha256
+		cd ..
+		# password="233333"
+		password=`GeneratePW`
+		# /root/rar/rar a -htb -m0 -ma5 -rr5 -ts -hp"$password" "$folder.rar" $folder
+		currentdate=`date +%y.%m.%d`
+		$RARpath a -df -v1g -htb -m0 -ma5 -rr5 -ts -ol -os -hp"$password" "$1.$currentdate.rar" $1
+		echo "$1|$currentdate|$password" >> pair.txt
+		echo "$1|$currentdate|$password" > "pair.$1.$currentdate.txt"
+	fi
+}
+
+if [ -z $1 ]
+then
+	for folder in `ls`
+	do
+		encrypt $folder
+	done
+else
+	encrypt $1
+fi
