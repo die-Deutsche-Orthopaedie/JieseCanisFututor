d10b41bd8f5eef2b02a2df888959a0ce3bf5694f
die♂Deutsche♂Orthopädie♂spezialist <amo@ludus.z0d.eu>
2016-07-13 14:22:19 +0800

    Create randomize.filename.sh
    now baidu disk can't even fuckin' know your filename:joy::joy_cat::joy::joy_cat::joy:...
    现在百度盘连你的文件名都™不知道了:joy::joy_cat::joy::joy_cat::joy:。。。

---
+++ randomize.filename.sh
@@ -0,0 +1,18 @@
+function GenerateString { 
+	len=32
+	str=(a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 短 发 大 法 好)
+	for((i=1;i<=$len;i++))
+	do
+		arr[$i]=${str[$[$RANDOM%87]]}
+	done
+	echo "${arr[@]}"|tr -d " "
+}
+
+for file in `echo *.rar`
+do
+	newname=`GenerateString`
+	newname="$newname.rar"
+	mv $file $newname
+	echo "$file|$newname" >> recover.txt
+	echo "mv \"$newname\" \"$file\"" >> recover.sh
+done
