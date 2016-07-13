ceb253da52e14dbe1bb1434661186baf297399ef
die♂Deutsche♂Orthopädie♂spezialist <amo@ludus.z0d.eu>
2016-07-13 11:40:59 +0800

    Create decrypt.v2.sh
    compatible with cygwin, now you can use the script on Windows. don't forget to copy a WinRAR into cygwin folder. 
    兼容cygwin，现在你可以在Windows上面使用这个脚本了。别忘了往cygwin里面拷一份WinRAR。

---
+++ decrypt.v2.sh
@@ -0,0 +1,30 @@
+RARpath="/home/ddOs/WinRAR/rar"
+
+for pair in `cat pair.txt`
+do
+	folder=`echo $pair | cut -d\| -f1`
+	currentdate=`echo $pair | cut -d\| -f2`
+	password=`echo $pair | cut -d\| -f3`
+	# /root/rar/rar x -ts -p"$password" "$folder.$currentdaterar"
+	if [ -f "$folder.$currentdate.rar" ]
+	then
+		$RARpath x -ts -p"$password" "$folder.$currentdate.rar"
+	fi
+	if [ -f "$folder.$currentdate.part1.rar" ]
+	then
+		$RARpath x -ts -p"$password" "$folder.$currentdate.part1.rar"
+	fi
+	if [ -f "$folder.$currentdate.part01.rar" ]
+	then
+		$RARpath x -ts -p"$password" "$folder.$currentdate.part01.rar"
+	fi
+	if [ -f "$folder.$currentdate.part001.rar" ]
+	then
+		$RARpath x -ts -p"$password" "$folder.$currentdate.part001.rar"
+	fi
+	cd $folder
+	md5sum --quiet -c $folder.md5
+	sha1sum --quiet -c $folder.sha1
+	sha256sum --quiet -c $folder.sha256
+	cd ..
+done
