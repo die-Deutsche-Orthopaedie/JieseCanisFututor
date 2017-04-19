030a49451e017a23128c00ab1eeba14e8f55a25b
die♂Deutsche♂Orthopädie♂spezialist <amo@ludus.z0d.eu>
2017-04-19 10:38:04 +0800

    Create convert.sh
    Convert recover.sh and pair.txt into a batch file (or we can say constract a batch file usin' contents of the aforementioned files). Not everyone wanna install and configure Cygwin for some porns, like this Orthopädiespezialist. 
    将recover.sh和pair.txt转化成一个批处理文件（或者说利用这两个文件的内容构造出一个批处理文件出来）。不是所有人都愿意为解压个片子像这位骨科专家一样装个Cygwin的。
    
    known issues: some archives can't extract due to password error. God knows why. Recommended to find password from batch file and extract normally. 
    已知的问题：有些压缩包解压不了，说密码是做的。鬼知道为什么，反正批处理怎么搞怎么恶心，自从用了Cygwin我已经好几年没碰过批处理了。建议从批处理文件里找出密码然后手动解压（不会连WinRAR都没装吧）。

---
+++ convert.sh
@@ -0,0 +1,18 @@
+cat recover.sh | sed "s/mv /ren /g" > extract.utf8.bat
+echo "copy \"C:\Program Files\WinRAR\rar.exe\"" >> extract.utf8.bat
+echo "if not exist copy \"C:\Program Files (x86)\WinRAR\rar.exe\"" >> extract.utf8.bat
+for pair in `cat pair.txt | sed 's/ /™/g'`
+do
+	folder=`echo $pair | cut -d\| -f1  | sed 's/™/ /g'`
+	currentdate=`echo $pair | cut -d\| -f2  | sed 's/™/ /g'`
+	password=`echo $pair | cut -d\| -f3  | sed 's/™/ /g'`
+    echo "if exist \"$folder.$currentdate.rar\" rar x -ts -p\"$password\" \"$folder.$currentdate.rar\"" >> extract.utf8.bat
+    echo "if exist \"$folder.$currentdate.part1.rar\" rar x -ts -p\"$password\" \"$folder.$currentdate.part1.rar\"" >> extract.utf8.bat
+    echo "if exist \"$folder.$currentdate.part01.rar\" rar x -ts -p\"$password\" \"$folder.$currentdate.part01.rar\"" >> extract.utf8.bat
+    # echo "rar x -ts -p\"$password\" \"$folder.$currentdate.part01.rar\"" >> extract.utf8.bat
+    echo "if exist \"$folder.$currentdate.part001.rar\" rar x -ts -p\"$password\" \"$folder.$currentdate.part001.rar\"" >> extract.utf8.bat
+    echo "if exist \"$folder.$currentdate.part0001.rar\" rar x -ts -p\"$password\" \"$folder.$currentdate.part0001.rar\"" >> extract.utf8.bat
+    # echo "del \"$folder.$currentdate.\"*rar" >> extract.utf8.bat
+done
+unix2dos extract.utf8.bat
+iconv -f UTF-8 -t GBK extract.utf8.bat > extract.bat
